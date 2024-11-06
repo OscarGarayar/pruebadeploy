8fd13ceba4cf0676e9230072605f6c870aa54042
Oscar Nathaniel Garayar Mori <102541011+OscarGarayar@users.noreply.github.com>
2024-11-05 22:31:04 -0500

    a

--- vite.config.mjs
+++ vite.config.mjs
@@ -7,6 +7,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: './', // Configura la base para rutas relativas en producción
     optimizeDeps: {
         noDiscovery: true
     },
